b68e96cf9b2d0117eb38f1c2feece957ffc5a804
unknown <joseparanan@gmail.com>
2021-04-29 23:10:04 -0300

    Projeto e rotas criados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"inicio", pathMatch:"full"},
+
+  {path:"blog", component:BlogComponent},
+  {path:"footer", component:FooterComponent},
+  {path:"inicio", component:InicioComponent},
+  {path:"menu", component:MenuComponent},
+  {path:"produtos", component:ProdutosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlogComponent } from './blog/blog.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    BlogComponent,
+    InicioComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
